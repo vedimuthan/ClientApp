f9da01fe632a341192fc8cd7b0da037a98436801
vedimuthan <vedimuthan1987@gmail.com>
2020-02-21 14:04:36 -0500

    Modified url

--- src/com/iaascloudsolutions/clientapp/ClientApp.java
+++ src/com/iaascloudsolutions/clientapp/ClientApp.java
@@ -13,10 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ClientApp
  */
-@WebServlet("/clientApp")
+@WebServlet("/clientapp")
 public class ClientApp extends HttpServlet {
     private static final long serialVersionUID = 1L;
-      
+
     /**
      * @see HttpServlet#HttpServlet()
      */
